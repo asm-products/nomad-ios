0604c68f4192caf956a3fba64d352998d1408707
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2014-10-30 19:09:22 +0100

    Created initial project with CocoaPods, Swift and a simple view controller.

---
+++ NomadTests/NomadTests.swift
@@ -0,0 +1,36 @@
+//
+//  NomadTests.swift
+//  NomadTests
+//
+//  Created by Jeroen Leenarts on 30-10-14.
+//  Copyright (c) 2014 Nomad. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NomadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+# platform :ios, '8.1'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'Nomad' do
+
+end
+
+target 'NomadTests' do
+
+end
+
